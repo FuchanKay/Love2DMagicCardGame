d32589e953fdf17ebeb391e8db89397ef9003031
FuchanKay <kazeuma0923@gmail.com>
2025-04-25 21:30:05 -0400

    updated fireball

--- game_objects/spells/spells.lua
+++ game_objects/spells/spells.lua
@@ -4,22 +4,26 @@ local CardTypes = require "game_objects.cards.card_types"
 
 spell_module.fire_ball = {
     name = "Fireball",
-    description = "Discard 1 card.\n\nDo 10 dmg to all enemies. Double if discarded card is arcane type",
+    description = "Discard 2 cards.\n\nDo 10 dmg to all enemies.\n\nDeal additional 10 dmg for every arcane card discarded.",
     effect = function ()
         local controller = spell_module.controller
         if not controller then error "controller is nil" end
-        local arcane_spell_cost = 2
-        if controller.resource_display.arcane_num >= arcane_spell_cost then
-            controller.subtractResource(CardTypes.arcane, arcane_spell_cost)
-            controller.forceDiscardCard(1)
+        local ARCANE_COST = 2
+        local DISCARD_NUM = 2
+        if controller.resource_display.arcane_num >= ARCANE_COST then
+            controller.subtractResource(CardTypes.arcane, ARCANE_COST)
+            controller.forceDiscardCard(2)
             local fn = function ()
-                local card = controller.hand.discarded_cards[#controller.hand.discarded_cards]
-                if card.type == CardTypes.arcane then controller.aoeUpdateHp(-20)
-                else controller.aoeUpdateHp(-10) end
+                local dmg = -10
+                for i = 0, DISCARD_NUM - 1 do
+                    local card = controller.hand.discarded_cards[#controller.hand.discarded_cards - i]
+                    if card.type == CardTypes.arcane then dmg = dmg - 10 end
+                end
+                controller.aoeUpdateHp(dmg)
             end
             controller.addEvent(fn)
         end
-    end,
+    end
 }
 
 return spell_module
